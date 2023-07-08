6e84d1acaead5eff7b9c298efdf89d89ef3ccc90
romzes2f <roman.v.shchebet@gmail.com>
2023-07-08 13:28:26 +0200

    ПР10

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mesto-react
+# Проект: Место React
+
+* [GitHub Pages](https://romzes2f.github.io/mesto-react/)
+
+### Технологии
+
+* HTML 5
+* CSS 3
+* JS
+* React
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__paragraph">
+                &copy; 2023. Роман Щебетенко
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
